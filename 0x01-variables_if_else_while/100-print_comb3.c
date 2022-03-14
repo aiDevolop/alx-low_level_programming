82b22cc7a04eeee2e148656d4ff2b94b19add8ef
aiDevolop <aidevolop@gmail.com>
2022-03-15 00:08:33 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,17 +10,19 @@ int main(void)
 {
 	int d;
 
-	for (d = 0; d < 100; d++)
+	for (d = 0; d < 90; d++)
 	{
-		putchar((d / 10) + '0');
-		putchar((d % 10) + '0');
-		if (d != 99)
+		for ()
+		{
+			putchar((d / 10) + '0');
+			putchar((d % 10) + '0');
+		}
+		if (d != 89)
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
