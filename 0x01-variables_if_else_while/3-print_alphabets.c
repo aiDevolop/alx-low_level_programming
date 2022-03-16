3373b95ce79eb2b52b6f9a83a8f9854b8d33e545
aiDevolop <aidevolop@gmail.com>
2022-03-16 22:00:06 +0100

    updated file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -36,17 +36,17 @@ int main(void)
 char lower_case;
 char upper_case;
 
-for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-{
-putchar(upper_case);
-}
 for (lower_case = 'a'; lower_case < 'z'; lower_case++)
 {
 putchar(lower_case);
 }
 
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+{
+putchar(upper_case);
+}
+
 putchar('\n');
->>>>>>> 2f48a52d2aefcb1ed6f9c68ce9209bdecfd92449
 return (0);
 
 }
