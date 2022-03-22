8a63b3c62d5e7dac2e5180fac9064a46e613c719
aiDevolop <aidevolop@gmail.com>
2022-03-22 13:21:17 +0000

    adding 0-reset_to_98.c file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - update an integer value
+ *@n: A pointer to an int that will be updated
+ *
+ *Return: voif that meanss is correct
+ */
+
+vooid reset_to _98(int *n)
+{
+*n = 98;
+}
+
