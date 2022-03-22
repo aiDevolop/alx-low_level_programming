8dc3fa4ebfd1dfac53c63e5124b158cb6eb1e090
aiDevolop <aidevolop@gmail.com>
2022-03-22 14:00:43 +0000

    adding 2-strlen file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strlen _length of a string
+ *@s: A pointer to sn int that could be changed or updated
+ *
+ *Return: void that means our answer is correct
+ */
+
+int _strlen(char *s)
+{
+int i;
+
+i = 0;
+while (s[i] !='\0')
+{
+i++;
+}
+return (i);
+}
+
