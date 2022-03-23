9dc79396348c4ca8247646a91ebaa0c6d7270a64
Yenou Isaiah Ayomide <97550820+aiDevolop@users.noreply.github.com>
2022-03-24 00:17:40 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,43 +1,38 @@
 #include "main.h"
+
 /**
-*cap_string - a function that capitalizes all words of a string.
-*
-*@str: Pointer to the array.
-*
-*Return: string pointer.
-*/
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be capitalized.
+ *
+ * Return: A pointer to the changed string.
+ */
 char *cap_string(char *str)
 {
-	int i = 0, j;
-	char cap[] = ";.!?\"()\t{}\n ";
-	int len = _strlen(str);
+	int index = 0;
 
-	if (str[i] >= 97 && str[i] <= 122)
+	while (str[index])
 	{
-		str[i] -= 32;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
-	for (i = 0; i < len; ++i)
-	{
-		for (j = 0; j <  _strlen(cap); ++j)
-		{
-			if (str[i] == cap[j] && str[i + 1] >= 97 && str[i + 1] <= 122)
-			{
-				str[i + 1] -= 32;
-			}
-		}
-	}
+
 	return (str);
-}
-/**
-* _strlen - function that that returns the length of a string.
-* @s : pointer to a string.
-* Return: string lenght
-*/
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
 }
