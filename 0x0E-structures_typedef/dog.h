32af251eee12699cd375827d667a13752c865737
aiDevolop <aidevolop@gmail.com>
2022-04-14 03:02:07 +0100

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure with arguments for it's name, age, and owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the name of the dog's owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
